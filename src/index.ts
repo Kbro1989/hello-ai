260e7953ef6703a3ba73a81f41725d5f253ebe96
Kbro1989 <149967879+Kbro1989@users.noreply.github.com>
2025-10-30 04:16:49 -0500

    Update to Workers AI tutorial

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+     interface Env {
+       AI: any;
+     }
+    
+     async function fetch(request: Request, env: Env): Promise<Response> {
+       const response = await env.AI.run("@cf/meta/llama-3.1-8b-instruct", {
+         prompt: "What is the origin of the phrase Hello, World",
+       });
+    
+      return new Response(JSON.stringify(response));
+    }
+   
+    export default {
+      fetch,
+    };
